f79e164e16e064151307b49f6ea0abf4d409e229
SIGENPOWER\100760 <fanghairong@sigenergy.com>
2025-03-20 16:05:54 +0800

    1

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -1,25 +1,8 @@
 import Pie from './world/Pie';
 
-/*
-//注解：three.js 占用的空间
-const dom: HTMLElement = document.querySelector('#earth-canvas');
-
-//注解：回调事件处理
-const callback = (params: any) => {
-  alert(JSON.stringify(params));
-}
-
-//注解：初始化整个three.js空间
-new World({
-  dom,
-  callback
-})
-*/
-
 //隐藏loading
 const loading = document.querySelector('#loading');
 loading.classList.add('out');
-
 
 //注解：three.js 占用的空间
 const dom: HTMLElement = document.querySelector('#pie-container');
